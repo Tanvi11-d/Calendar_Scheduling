6a8c22dd6d0376881a7ad24b31e2b0f561b73bd3
Tanvi11-d <Tanvi.d@bitcodingsolutions.com>
2026-03-24 18:38:09 +0530

    feat:add update,check availability,get event

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def get_schedule(query:str):
     try:
         result=tool_result(query)
         answer=result.get('messages')[-1].content
+        
         return {"answer":answer}
      
     except Exception as e:
--- utils.py
+++ utils.py
@@ -17,90 +17,152 @@ api_key=os.getenv("GROQ_API_KEY")
 
 schedule="schedule.json"
 
-
 # create event tool
-
 @tool
-def create_event(title,duration_minutes=60):
-    """add a new events"""   
+def create_event(title:str,time,duration_minutes=60):
+    """create a new events."""   
     try:
+        try:
+            dt=datetime.fromisoformat(time)
+        except:
+            dt=datetime.strftime(time,"%d-%B-%Y %I%p")
+        isoformat_date=dt.isoformat()
+
         if os.path.exists(schedule) and os.path.getsize(schedule)>0:
             with open(schedule,'r') as file:
                 schedules=json.load(file)
+            
         else:
             schedules=[]
-        todays_Date = datetime.now()
-        isoformat_date = todays_Date.isoformat()
+
+        for i in schedules:
+            if i["time"]==isoformat_date and i["title"].lower()==title.lower() and i["duration_minutes"]==duration_minutes:
+                return "Event already exists"
+            
         event={'title':title,'time':isoformat_date,'duration_minutes':duration_minutes}
         schedules.append(event)
         with open(schedule,'w') as file:
             json.dump(schedules,file,indent=4)
                    
         log.info("Event created")  
-        return f"Event created"    
+        return f"{title} at {time}"
+        
     except Exception as e:
         log.error("Event not created")
         raise HTTPException(status_code=500,detail=f"error in create event {e}")
-    
+   
 @tool
-def get_events():
-    """ show all events."""
+def get_events(date):
+    """get event sorted by start time."""
     try:
+
         with open(schedule,'r') as file:
             get_event=json.load(file)
-        result=" "
-        date_str=datetime.now().strftime("%Y-%m-%d")
 
+        result=[]
+        date=datetime.now().strftime("%Y-%m-%d")
+
+        print("date___",date)
+
+        for i in get_event:
+            if i["time"].startswith(date):
+                result.append(i)
+
+        print("get_event__",i)
+
+
+        def get_item(item):
+            return item["time"]
+        result.sort(key=get_item)
+
+        output=" "
         for i,t in enumerate(get_event,start=1):
-            result+=f"{i}.{t['title']+ date_str}"
-
+            output+=f"{i}.{t["title"]} {t["time"]}\n"
+        
         log.info("Show Events")
-        return f"result{result}"
+        print("output__",output)
+        return output
     
     except Exception as e:
         log.error("Events not found")
         raise HTTPException(status_code=500,detail=f"error in get_events{e}")
     
-
 @tool
-def check_availability(time,duration_minutes=60):
-    """ask available slot """
+def check_availability(time):
+    """check time slot available or not.return available slot and tell next free slot when taken.""" 
     try:
+
         with open(schedule,'r') as file:
             schedules=json.load(file)
-        
-
-        
-        
+        for i in schedules:
+            if i["time"]==time:
+                return False
+        log.info("slot available")
+        return True
+    
     except Exception as e:
+        log.error("slot not available")
         raise HTTPException(status_code=500,detail=f"error in check_availability {e}")
 
 
 @tool
 def update_event(old_time,new_time):
-    """update an events """
+    """update an existing events.Do not delete or recreate events."""
     try:
+        
         with open(schedule,'r') as file:
             schedules=json.load(file)
+            
+        for i in schedules:
+            if i["time"]==old_time:
+                i["time"]=new_time
+
+                with open(schedule,'w') as file:
+                    json.dump(schedules,file,indent=4)
+                return "event updated"
+
+        log.info("event updated")
+        return "event not updated"
         
-        old_time=datetime.now().fromisoformat()
-        new_time=datetime.now().isoformat()
-
-
     except Exception as e:
         log.error("Event not updated")
         raise HTTPException(status_code=500,detail=f"error in update_event {e}")
     
 system_prompt=f"""
-- you are  Scheduling Assistant.
+- You are Calendar Scheduling Assistant.
 - you are follow the below rules.
 
 Rules:
-1.if user asks create new events then call create_event tool,only respond  "Event created"
-2.if you are show events or query schedule then call get_events.sorted by start time.
-3.give answer polity and natural language.
-4.if you are update events then call update_event tool.you are not delete or recreate events.
-5.if user schedule multiple events then add one by one in json.
+
+1.create events
+- create a new events -> call create_event.
+- If user not define start time of events then take anything start time for that day and default duration minutes is 60.
+- if user give range of events then store in separatly.
+- do not create duplicates events.
+- if user give date of range events(example:- create schedule 1 april to 5 april),
+    i)create separate event one by one with same time and title.
+    
+2.get events
+- show events or query schedule -> call get_events.
+- it sorted by start time and Default date is today.
+- if you are show event sorted by start time.
+- when you are show all events then return with number format.
+
+3.check availability
+- check slot available or not -> call check_availability.
+- return which slot available and tell next free slot when taken.
+
+4.update event
+- update events -> call update_event.
+- update existing event slot to new slots based on datetime.
+- you are not delete or recreate events.
+
+5.others
+- keep answer polity,concise and natural language.
+- Do not give extra information.
+- Do not show which tool you used.
+- Do not answer general knowledge questions.
+- Do not hallucinate create events,check availability or update events.
 
 """
 
@@ -120,6 +182,7 @@ agent=create_agent(
 def tool_result(query:str):
     try:  
         response=agent.invoke({'messages':[AIMessage(content=query)]})
+        log.info("tool_result callable")
         return response
     except Exception as e:
         log.error("tool_result is not callable")
